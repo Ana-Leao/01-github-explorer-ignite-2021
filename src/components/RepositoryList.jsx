f0c5831cc00d62d20a9d70ce65cee3f07fd6a603
Ana Paula Leão <anaqueiroz.dev@gmail.com>
2023-04-09 18:20:39 -0300

    api github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,30 @@
 import RepositoryItem from './RepositoryItem';
 import '../styles/repositories.scss';
-
-const repository = {
-   name: 'unform',
-   description: 'Forms in React',
-   link: 'https://github.com/Ana-Leao',
-};
+import { useEffect, useState } from 'react';
 
 function RepositoryList() {
+   const [repositories, setRepositories] = useState([]);
+
+   useEffect(() => {
+      fetch('https://api.github.com/users/Ana-Leao/repos')
+         .then((response) => response.json())
+         .then((data) => setRepositories(data));
+   }, []);
+
    return (
       <section className='repository-list'>
          <h1>Lista de Repositórios</h1>
          <ul>
-            <RepositoryItem repository={repository} />
-            <RepositoryItem repository={repository} />
-            <RepositoryItem repository={repository} />
-            <RepositoryItem repository={repository} />
+            {repositories.map((repository) => {
+               return (
+                  <RepositoryItem
+                     key={repository.name}
+                     repository={repository}
+                  />
+               );
+            })}
+
+            {/* {repositories.map((repository) => (<RepositoryItem {repository.name} repository={repository}/>))} */}
          </ul>
       </section>
    );
